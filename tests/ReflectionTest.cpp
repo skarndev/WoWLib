19897afb2b0b4f87137bdbf08d6d3ec2b8bbf556
Skarn <skarnproject@gmail.com>
2022-09-04 16:25:02 +0300

    start incorporating new inline loops into existing meta code | reflection add new AnyOf construct.

--- tests/ReflectionTest.cpp
+++ tests/ReflectionTest.cpp
@@ -19,9 +19,9 @@ int main()
   static_assert(!Concepts::Reflectable<TestNonReflectable>);
   static_assert(Concepts::Reflectable<Test>);
 
-  bool has = Reflect<Test>::HasMember("first");
+  constexpr bool has = Reflect<Test>::HasMember<"first1">();
 
-  if (!has)
+  if constexpr (!has)
     return 1;
 
   auto ptr = Reflect<Test>::GetMemberPtr<"first">();
